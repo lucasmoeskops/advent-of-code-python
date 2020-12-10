4eb6be10c7e7400324113e2ba8b8f32deea7dfc5
Lucas Moeskops <lucasmoeskops@gmail.com>
2020-12-10 21:53:15 +0100

    minor tweaks

--- src/y2020/10. Adapter Array.py
+++ src/y2020/10. Adapter Array.py
@@ -3,11 +3,11 @@ from itertools import groupby
 from math import prod
 from sys import stdin
 
-diffs = lambda ns: map(lambda i: ns[i] - ns[i - 1], range(1, len(ns)))
 count = lambda i: 1 + (i - 1) * i // 2
 
 lines = stdin.read().split('\n')
 ns = [0, *(sn := sorted(map(int, lines))), sn[-1] + 3]
+diffs = [j - i for i, j in zip(ns[:-1], ns[1:])]
 
-print(f'1: {prod(Counter(diffs(ns)).values())}')
-print(f'2: {prod(count(len([*g])) for k, g in groupby(diffs(ns)) if k == 1)}')
+print(f'1: {prod(Counter(diffs).values())}')
+print(f'2: {prod(count(len([*g])) for k, g in groupby(diffs) if k == 1)}')
