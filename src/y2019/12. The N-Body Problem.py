495c19c13eccdf07b61b972b79fa027380b04975
Lucas Moeskops <lucasmoeskops@gmail.com>
2021-09-04 11:08:57 +0200

    Tweaks

--- src/y2019/12. The N-Body Problem.py
+++ src/y2019/12. The N-Body Problem.py
@@ -6,6 +6,7 @@ __author__ = "Lucas Moeskops"
 __date__ = "2021-02-07"
 
 from functools import reduce
+from itertools import islice
 from operator import itemgetter
 from math import gcd, prod
 from sys import stdin
@@ -25,14 +26,20 @@ def apply_gravity(moons):
         for j, (ox, oy, oz, _, _, _) in enumerate(moons):
             if i == j:
                 continue
-            vx += 0 if x == ox else (1 if x < ox else -1)
-            vy += 0 if y == oy else (1 if y < oy else -1)
-            vz += 0 if z == oz else (1 if z < oz else -1)
+            vx += (x < ox) - (x > ox)
+            vy += (y < oy) - (y > oy)
+            vz += (z < oz) - (z > oz)
         yield Moon(x, y, z, vx, vy, vz)
 
 def apply_velocity(moons):
     for x, y, z, vx, vy, vz in moons:
         yield Moon(x + vx, y + vy, z + vz, vx, vy, vz)
+
+def state(moons):
+    while True:
+        yield moons
+        moons = list(apply_gravity(moons))
+        moons = list(apply_velocity(moons))
 
 def potential_energy(moon):
     x, y, z, _, _, _ = moon
@@ -45,26 +52,21 @@ def kinetic_energy(moon):
 def total_energy(moons):
     return sum(potential_energy(moon) * kinetic_energy(moon) for moon in moons)
 
-def simulate(moons, rounds):
-    for i in range(rounds):
-        moons = list(apply_velocity(apply_gravity(moons)))
-    return moons
-
 moons_raw = stdin.read().split('\n')
 moons_raw = (reduce(lambda s, c: s.replace(c, ''), '<> ', moon) for moon in moons_raw)
 moons_raw = (moon.split(',') for moon in moons_raw)
 moons = [Moon(*(int(value.split('=')[1]) for value in moon)) for moon in moons_raw]
-moons = simulate(moons, 1000)
+moons_after_1000 = next(islice(state(moons), 1000, 1001))
 
-print(f'1: {total_energy(moons)}')
+print(f'1: {total_energy(moons_after_1000)}')
 
 def apply_gravity_single(moon_aspects):
     for i, (x, vx) in enumerate(moon_aspects):
         for j, (ox, _) in enumerate(moon_aspects):
             if i == j:
                 continue
-            vx += 0 if x == ox else (1 if x < ox else -1)
-        yield (x, vx)
+            vx += (x < ox) - (x > ox)
+        yield x, vx
 
 def apply_velocity_single(moon_aspects):
     for x, vx in moon_aspects:
@@ -75,16 +77,14 @@ def iteration_count_per_dimension(moons):
         getter = itemgetter(dimension, dimension + 3)
         moon_aspects = list(map(getter, moons))
         initial = moon_aspects[:]
-        iterations = 1
-        while True:
-            moon_aspects = apply_gravity_single(moon_aspects)
-            moon_aspects = apply_velocity_single(moon_aspects)
-            moon_aspects = list(moon_aspects)
+        iterations = 0
 
-            if moon_aspects == initial:
-                yield iterations
-                break
+        while any(map(itemgetter(1), moon_aspects)) or not iterations:
+            moon_aspects = list(apply_gravity_single(moon_aspects))
+            moon_aspects = list(apply_velocity_single(moon_aspects))
             iterations += 1
+            
+        yield iterations * 2
 
 def find_cycle_length(moons):
     counts = list(iteration_count_per_dimension(moons))
