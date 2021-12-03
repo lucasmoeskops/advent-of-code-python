4b49161e8fe27f775a6e4637fd3f436f76bb4778
Lucas Moeskops <lucas@fabrique.nl>
2021-12-03 08:07:33 +0100

    clean up

--- src/y2021/03. Binary Diagnostics.py
+++ src/y2021/03. Binary Diagnostics.py
@@ -17,15 +17,11 @@ full = stdin.read()
 lines = full.split('\n')
 
 
-def str2bin(s):
-    return int(''.join(s), 2)
-
-
 @timed
 def task_1():
     gamma_rate = ''.join(max('01', key=p.count) for p in zip(*lines))
     epsilon_rate = gamma_rate.replace('0', '2').replace('1', '0').replace('2', '1')
-    return str2bin(gamma_rate) * str2bin(epsilon_rate)
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
 
 
 def build_reducer(_filter_, order):
@@ -41,7 +37,7 @@ def task_2():
     n = len(lines[0])
     oxygen_generator_rating = reduce(build_reducer(max, '10'), range(n), lines)[0]
     co2_scrubber_rating = reduce(build_reducer(min, '01'), range(n), lines)[0]
-    return str2bin(oxygen_generator_rating) * str2bin(co2_scrubber_rating)
+    return int(oxygen_generator_rating, 2) * int(co2_scrubber_rating, 2)
 
 
 print(f'[Part 1]: {task_1()}')
