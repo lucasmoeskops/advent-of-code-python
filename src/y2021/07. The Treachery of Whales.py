8e3c45d070978ad94c0080774f401bb5de814684
Lucas Moeskops <lucas@fabrique.nl>
2021-12-07 06:43:27 +0100

    Day 7 tweaks

--- src/y2021/07. The Treachery of Whales.py
+++ src/y2021/07. The Treachery of Whales.py
@@ -14,11 +14,10 @@ from helpers import timed
 positions = [int(v) for v in stdin.read().split(',')]
 
 
-def calc_best(f):
-    avg = int(sum(positions) / len(positions))
-    base = f(avg)
-    less, prev = avg - 1, base
-    more, nxt = avg + 1, base
+def calc_best(f, s):
+    base = f(s)
+    less, prev = s - 1, base
+    more, nxt = s + 1, base
     while (new := f(less)) < prev:
         less -= 1
         prev = new
@@ -30,13 +29,17 @@ def calc_best(f):
 
 @timed
 def task_1():
-    return calc_best(lambda t: sum(abs(s - t) for s in positions))
+    return calc_best(
+        lambda t: sum(abs(s - t) for s in positions),
+        sorted(positions)[len(positions) // 2],
+    )
 
 
 @timed
 def task_2():
     return calc_best(
-        lambda t: sum(abs(s - t) * (abs(s - t) + 1) // 2 for s in positions)
+        lambda t: sum(abs(s - t) * (abs(s - t) + 1) // 2 for s in positions),
+        int(sum(positions) / len(positions)),
     )
 
 
