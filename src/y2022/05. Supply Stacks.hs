95e31fbfe8a6c94ed086b2ecc1d6e1baeaaa1bb6
lucas <lucas@fabrique.nl>
2022-12-05 19:57:41 +0100

    Use Text more

--- src/y2022/05. Supply Stacks.hs
+++ src/y2022/05. Supply Stacks.hs
@@ -1,64 +1,72 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.List (transpose)
-import Data.Text (Text, pack, splitOn, strip, unpack)
+import Data.Text.IO as IO (getContents)
+import Data.Text (Text)
+import qualified Data.Text as T
 
 
 data Version = V9000 | V9001 deriving Eq
 
 type Command = (Int, Int, Int)
 type Crate = Char
-type CrateSetup = [[Crate]]
+type CrateSetup = [Text]
 
 
-useCrateMover :: Version -> CrateSetup -> [Command] -> String
-useCrateMover version stacks [] = map head stacks
+useCrateMover :: Version -> CrateSetup -> [Command] -> Text
+useCrateMover version stacks [] = T.pack . map T.head $ stacks
 useCrateMover version stacks ((amount, from, to):ms) = useCrateMover version (move [] 1 stacks) ms
     where
         move :: CrateSetup -> Int -> CrateSetup -> CrateSetup
         move newStacks n [] = reverse newStacks
         move newStacks n oldStacks =
-            let action | n == from = drop amount
-                       | n == to   = ((adjust . take amount . (!!(from - 1)) $ stacks)++)
+            let action | n == from = T.drop amount
+                       | n == to   = T.append . adjust . T.take amount . (!!(from - 1)) $ stacks
                        | otherwise = id
             in move ((action . head $ oldStacks):newStacks) (n+1) (drop 1 oldStacks)
 
-        adjust :: [a] -> [a]
-        adjust = if version == V9000 then reverse else id
+        adjust :: Text -> Text
+        adjust = if version == V9000 then T.reverse else id
 
 
-parse :: String -> (CrateSetup, [Command])
+parse :: Text -> (CrateSetup, [Command])
 parse contents =
-    let [stacks, movements] = splitOn (pack "\n\n") . pack $ contents
+    let [stacks, movements] = T.splitOn "\n\n" $ contents
     in (parseStacks stacks, parseCommands movements)
 
 
 parseStacks :: Text -> CrateSetup
 parseStacks =
-      map (init . dropWhile (==' '))
-    . every 4
-    . (["", ""]++)
-    . transpose
-    . map unpack
-    . splitOn (pack "\n")
+      map (T.init . T.dropWhile (==' '))
+    . everyWithOffset 4 1
+    . T.transpose
+    . T.splitOn "\n"
 
 
 parseCommands :: Text -> [Command]
-parseCommands = map parseCommand . splitOn (pack "\n") . strip
+parseCommands = map parseCommand . T.splitOn "\n" . T.strip
 
 
-parseCommand :: Text -> Command
+parseCommand :: T.Text -> Command
 parseCommand s =
-    let parts = map unpack . splitOn (pack " ") $ s
+    let parts = map T.unpack . T.splitOn " " $ s
     in (read $ parts!!1, read $ parts!!3, read $ parts!!5)
 
 
-every :: Int -> [a] -> [a]
-every n xs =
+everyWithOffset :: Int -> Int -> [a] -> [a]
+everyWithOffset n o [] = []
+everyWithOffset n 0 (x:xs) = x : everyWithOffset n (-1) xs
+everyWithOffset n (-1) xs =
     case drop (n-1) xs of
-        y : ys -> y : every n ys
+        y : ys -> y : everyWithOffset n (-1) ys
+        []     -> []
+everyWithOffset n o xs =
+    case drop o xs of
+        y : ys -> y : everyWithOffset n (-1) ys
         []     -> []
 
 
 main = do
-    (stacks, movements) <- parse <$> getContents
+    (stacks, movements) <- parse <$> IO.getContents
     putStrLn . show $ useCrateMover V9000 stacks movements
     putStrLn . show $ useCrateMover V9001 stacks movements
