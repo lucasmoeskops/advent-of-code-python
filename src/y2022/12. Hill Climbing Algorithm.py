c65dc4a4bd351dfde1f56d32d60971342c6c7182
lucas <lucas@fabrique.nl>
2022-12-12 09:16:48 +0100

    Day 12

---
+++ src/y2022/12. Hill Climbing Algorithm.py
@@ -0,0 +1,35 @@
+from collections import deque
+from sys import stdin
+
+DATA = stdin.read()
+
+
+def shortest(start, end, descend=False):
+    queue = deque([(start, 0, ord('a' if DATA[start] == 'S' else 'z'))])
+    seen = {start}
+
+    while queue:
+        index, steps, elevation = queue.popleft()
+
+        for di in (width, 1, -width, -1):
+            p = index + di
+
+            if p not in seen:
+                if p < 0 or p + 1 > len(DATA):
+                    continue
+
+                target_elevation = ord(DATA[p] if DATA[p] != 'E' else 'z')
+
+                if (elevation - target_elevation if descend else target_elevation - elevation) < 2:
+                    if end and p == end or (descend and DATA[p] == 'a'):
+                        return steps + 1
+
+                    seen.add(p)
+                    queue.append((p, steps + 1, target_elevation))
+
+
+width = (DATA.index('\n') + 1)
+start, end = map(DATA.index, "SE")
+
+print(shortest(start, end))
+print(shortest(end, None, True))
