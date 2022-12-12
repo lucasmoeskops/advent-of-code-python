ab85d438765b0e3543f3609781075baeff7a4d61
lucas <lucas@fabrique.nl>
2022-12-12 10:44:01 +0100

    Shorter

--- src/y2022/12. Hill Climbing Algorithm.py
+++ src/y2022/12. Hill Climbing Algorithm.py
@@ -4,13 +4,13 @@ from sys import stdin
 DATA = stdin.read()
 
 
-def shortest(start, end, descend=False):
-    queue = deque([(start, 0)])
-    seen = {start}
+def shortest(for_part_2=False):
+    queue = deque([(end if for_part_2 else start, 0)])
+    seen = {end if for_part_2 else start}
 
     while queue:
         index, steps = queue.popleft()
-        elevation = ord(DATA[index] if steps else 'a' if DATA[index] == 'S' else 'z')
+        elevation = ord(DATA[index] if steps else 'z' if for_part_2 else 'a')
 
         for di in (width, 1, -width, -1):
             p = index + di
@@ -21,9 +21,9 @@ def shortest(start, end, descend=False):
 
                 target_elevation = ord(DATA[p] if DATA[p] != 'E' else 'z')
 
-                if (elevation - target_elevation if descend else target_elevation - elevation) < 2:
-                    if end and p == end or descend and DATA[p] == 'a':
-                        return steps + 1
+                if (elevation - target_elevation if for_part_2 else target_elevation - elevation) < 2:
+                    if for_part_2 and DATA[p] == 'a' or DATA[p] == 'E':
+                        return str(steps + 1)
 
                     seen.add(p)
                     queue.append((p, steps + 1))
@@ -31,6 +31,4 @@ def shortest(start, end, descend=False):
 
 width = DATA.index('\n') + 1
 start, end = map(DATA.index, "SE")
-
-print(shortest(start, end))
-print(shortest(end, None, True))
+print('\n'.join(map(shortest, [False, True])))
