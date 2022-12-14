7e6efc2ca2105ce051775e9b5a12b1f46abbac28
lucas <lucas@fabrique.nl>
2022-12-14 23:53:35 +0100

    Improve runtime by factor 23

--- src/y2022/14. Regolith Reservoir.py
+++ src/y2022/14. Regolith Reservoir.py
@@ -22,30 +22,46 @@ for line in DATA:
         x, y = dx, dy
 
 max_y = max(y for x, y in map_)
-count = 0
+at_rest = False
 
-# Loop first for infinite bottom then for fixed bottom
-for before_bottom in (max_y + 3, max_y + 1):
-    at_rest = False
+while not at_rest:
+    sx, sy = 500, 0
 
-    while not at_rest:
-        sx, sy = 500, 0
+    while True:
+        if sy > max_y:
+            at_rest = True
+            break
+        if map_[(sx, sy + 1)] == '.':
+            sy += 1
+        elif map_[(sx - 1, sy + 1)] == '.':
+            sx -= 1
+            sy += 1
+        elif map_[(sx + 1, sy + 1)] == '.':
+            sx += 1
+            sy += 1
+        else:
+            map_[(sx, sy)] = 'o'
+            break
 
-        while True:
-            if sy > max_y + 2:  # Check for infinity
-                at_rest = True
-                break
-            if sy < before_bottom and map_[(sx, sy + 1)] == '.':
-                sy += 1
-            elif sy < before_bottom and map_[(sx - 1, sy + 1)] == '.':
-                sx -= 1
-                sy += 1
-            elif sy < before_bottom and map_[(sx + 1, sy + 1)] == '.':
-                sx += 1
-                sy += 1
-            else:
-                map_[(sx, sy)] = 'o'
-                at_rest = sy == 0
-                break
+print(sum(1 for t in map_.values() if t == 'o'))
 
-    print(sum(1 for t in map_.values() if t == 'o'))
+count = 1
+bound_right, bound_left = -500, 500
+
+for y in range(1, max_y + 2):
+    streak = map_[(-y - 1, y-1)] != '#' + map_[(-y - 1, y-1)] != '#'
+    for x in range(-y + bound_left, y + 1 - bound_right):
+        streak = max(0, streak + 1 if map_[(x+1, y-1)] != '#' else 3)
+        if map_[(x, y)] == '#':
+            if map_[(x - 1, y)] == '#' == map_[(x + 1, y)]:
+                map_[(x, y + 1)] = '#'
+            if x == -y + bound_left:
+                bound_left += 1
+            elif x == y + 1 - bound_right:
+                bound_right += 1
+                while map_[(y + 1 - bound_right, y)] == '#':
+                    bound_right += 1
+        elif streak:
+            count += 1
+
+print(count)
