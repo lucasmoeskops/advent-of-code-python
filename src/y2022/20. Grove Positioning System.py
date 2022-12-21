b6d9918a448e5318ad63f1a21ee4a0ff9cd9ecf7
lucas <lucas@fabrique.nl>
2022-12-21 01:30:00 +0100

    4x faster day 20

--- src/y2022/20. Grove Positioning System.py
+++ src/y2022/20. Grove Positioning System.py
@@ -5,18 +5,21 @@ DATA = stdin.read().split('\n')
 
 ns = [int(x) for x in DATA]
 length = len(ns)
-target = ns.index(0), 0
+mod_length = length - 1
+target = ns.index(0)
 
 for multiplier, num_rounds in ((1, 1), (811589153, 10)):
     sequence = [n * multiplier for n in ns]
-    numbers = deque(enumerate(sequence))
+    numbers = deque(range(length))
 
     for _ in range(num_rounds):
-        for tuple in zip(range(length), sequence):
-            old = numbers.index(tuple)
-            new = (old + tuple[1]) % (length - 1)
-            numbers.remove(tuple)
-            numbers.insert(new, tuple)
+        for i in range(length):
+            old = numbers.index(i)
+            new = sequence[i] % mod_length
+            numbers.rotate(-old)
+            numbers.popleft()
+            numbers.rotate(-new)
+            numbers.appendleft(i)
 
     start = numbers.index(target)
-    print(sum(numbers[(start + i) % len(numbers)][1] for i in (1000, 2000, 3000)))
+    print(sum(sequence[numbers[(start + i) % len(numbers)]] for i in (1000, 2000, 3000)))
