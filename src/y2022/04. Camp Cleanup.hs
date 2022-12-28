b6831da71b4617d65d3d7f03facbdcb480748f19
lucas <lucas@fabrique.nl>
2022-12-28 14:36:22 +0100

    Day 4 in Haskell

---
+++ src/y2022/04. Camp Cleanup.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Text (Text)
+import qualified Data.Text.IO as TIO (getContents)
+import qualified Data.Text as T (map, pack, splitOn, strip, unpack)
+
+
+type Range = (Int, Int)
+type Pair = (Range, Range)
+
+
+contains :: Range -> Range -> Bool
+contains (a, b) (c, d) = a <= c && b >= d || a >= c && b <= d
+
+
+overlaps :: Range -> Range -> Bool
+overlaps (a, b) (c, d) = a <= d && b >= c || a >= d && b <= c
+
+
+part1 :: [Pair] -> String
+part1 = show . length . filter (uncurry contains)
+
+
+part2 :: [Pair] -> String
+part2 = show . length . filter (uncurry overlaps)
+
+
+deserialize :: Text -> [Pair]
+deserialize =
+    let repl '-' = ','
+        repl c   = c
+        deserializeLine = combine . map (read . T.unpack) . T.splitOn ","
+        combine (a:b:c:d:es) = ((a, b), (c, d))
+    in map deserializeLine . T.splitOn "\n" . T.map repl . T.strip
+
+
+present = do putStrLn . foldr1 (\a b -> a ++ '\n':b)
+
+main = do
+    pairs <- deserialize <$> TIO.getContents
+    present [part1 pairs, part2 pairs]
