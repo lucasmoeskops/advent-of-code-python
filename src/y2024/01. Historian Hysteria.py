670a1cb2da79e796262cfbc5d160fe6dc86ab044
Lucas Moeskops <lucas@fabrique.nl>
2024-12-03 19:30:25 +0100

    2024 Dec 1

---
+++ src/y2024/01. Historian Hysteria.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+AoC Day 1 - Historian Hysteria - in Python.
+"""
+
+__author__ = "Lucas Moeskops"
+__date__ = "2024-12-01"
+__summary__ = "The Chief Historian is lost"
+
+from lib import *
+
+puzzle = read_input(2024, 1)
+numbers = map(int, puzzle.split())
+matrix = batched(numbers, 2)
+left, right = map(sorted, zip(*matrix))
+print(sum(abs(a - b) for a, b in zip(left, right)))
+
+occurrences = Counter(right)
+print(sum(num * occurrences[num] for num in left))
