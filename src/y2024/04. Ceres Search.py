2ab5fdf1c2485a95511aa840d6224af1526d05cf
Lucas Moeskops <lucas@fabrique.nl>
2024-12-07 12:31:13 +0100

    2024 Dec 4

---
+++ src/y2024/04. Ceres Search.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+AoC Day 4 - Ceres Search - in Python.
+"""
+
+__author__ = "Lucas Moeskops"
+__date__ = "2024-12-04"
+__summary__ = "He is not in the Ceres Monitoring Station"
+
+from lib import *
+
+
+puzzle = read_input(2024, 4)
+puzzle += '\n'
+w = puzzle.index('\n')
+regexes = [
+    r'XMAS',
+    r'SAMX',
+    rf'X(?=.{{{w}}}M.{{{w}}}A.{{{w}}}S)',
+    rf'S(?=.{{{w}}}A.{{{w}}}M.{{{w}}}X)',
+    rf'X(?=.{{{w+1}}}M.{{{w+1}}}A.{{{w+1}}}S)',
+    rf'S(?=.{{{w+1}}}A.{{{w+1}}}M.{{{w+1}}}X)',
+    rf'X(?=.{{{w-1}}}M.{{{w-1}}}A.{{{w-1}}}S)',
+    rf'S(?=.{{{w-1}}}A.{{{w-1}}}M.{{{w-1}}}X)',
+]
+print(sum(len(re.findall(regex, puzzle, re.M|re.DOTALL)) for regex in regexes))
+regexes = [
+    rf'M(?=.M.{{{w-1}}}A.{{{w-1}}}S.S)',
+    rf'M(?=.S.{{{w-1}}}A.{{{w-1}}}M.S)',
+    rf'S(?=.S.{{{w-1}}}A.{{{w-1}}}M.M)',
+    rf'S(?=.M.{{{w-1}}}A.{{{w-1}}}S.M)',
+]
+print(len(re.findall('|'.join(regexes), puzzle, re.M|re.DOTALL)))
