d60185a13cf81cbba7fdc2d729c0f4ced2179a0a
Lucas Moeskops <lucas@fabrique.nl>
2024-12-07 12:33:04 +0100

    2024 Dec 7

---
+++ src/y2024/07. Bridge Repair.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+"""
+AoC Day 7 - Bridge Repair - in Python.
+"""
+
+__author__ = "Lucas Moeskops"
+__date__ = "2024-12-07"
+__summary__ = "The chief isn't at the jungle rope bridge"
+
+from lib import *
+
+
+def concat(a, b):
+    return int(str(a) + str(b))
+
+
+def evaluator(operations):
+    def evaluate(nums, answer):
+        current = [nums[0]]
+        for num in nums[1:]:
+            new = []
+            for c in current:
+                for op in operations:
+                    if (result := op(c, num)) <= answer:
+                        new.append(result)
+            current = new
+        return answer in current
+    return evaluate
+
+
+puzzle = read_input(2024, 7)
+calibration_basic = evaluator((add, mul))
+calibration_advanced = evaluator((add, mul, concat))
+result_basic = 0
+result_advanced = 0
+
+for line in lines(puzzle.strip()):
+    result, *operators = map(int, line.replace(':', '').split())
+    if calibration_basic(operators, result):
+        result_basic += result
+    elif calibration_advanced(operators, result):
+        result_advanced += result
+
+print(result_basic)
+print(result_basic + result_advanced)
