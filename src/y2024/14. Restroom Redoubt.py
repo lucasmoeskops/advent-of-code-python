ffdeec540a6c1a7b3495f7c1b755b42668020588
Lucas Moeskops <lucas@fabrique.nl>
2024-12-19 17:28:26 +0100

    Faster dec 14

--- src/y2024/14. Restroom Redoubt.py
+++ src/y2024/14. Restroom Redoubt.py
@@ -8,19 +8,21 @@ __author__ = "Lucas Moeskops"
 __date__ = "2024-12-14"
 __summary__ = "Back to year 2016, day 2"
 
+from collections import Counter
 
-from lib import batched, prod, read_input, re
+from lib import batched, ints, prod, read_input, re
 
 
 puzzle = read_input(2024, 14)
-ints = map(int, re.findall(r'-?\d+', puzzle))
-robots = list(map(list, batched(ints, 4)))
+robots = list(map(list, batched(ints(puzzle), 4)))
+robots_clean = list(map(list, batched(ints(puzzle), 4)))
 width = 101
 height = 103
 best, best_at = 0, 0
+special = []
 
-for second in range(width * height):
-    filled = set()
+for second in range(height):
+    row_counter = Counter()
     correlation_score = 0
 
     if second == 100:
@@ -35,13 +37,29 @@ for second in range(width * height):
     for robot in robots:
         robot[0] = (robot[0] + robot[2]) % width
         robot[1] = (robot[1] + robot[3]) % height
-        filled.add((robot[0], robot[1]))
+        row_counter[robot[1]] += 1
 
-    for x, y in filled:
-        correlation_score += ((x-1, y) in filled)# + ((x, y-1) in filled)
-
-    if correlation_score > best:
-        best = correlation_score
+    if max(row_counter.values()) > best:
+        best = max(row_counter.values())
         best_at = second
+
+
+special = best_at
+
+for interval in (height, width):
+    for second in range(special, width * height, interval):
+        filled = set()
+        correlation_score = 0
+        second += 1
+
+        for robot in robots_clean:
+            filled.add(((robot[0] + robot[2] * second) % width, (robot[1] + robot[3] * second) % height))
+
+        for x, y in filled:
+            correlation_score += ((x, y-1) in filled)
+
+        if correlation_score > best:
+            best = correlation_score
+            best_at = second - 1
 
 print(best_at)
